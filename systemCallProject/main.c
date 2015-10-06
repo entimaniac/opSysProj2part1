b317b4568f8a980df9a6751ec019c8751bfec37f
jessestrode <jessedans91@gmail.com>
2015-10-06 09:41:55 -0400

    Played around for a while and got the strace to be 10 more than an empty program.

--- systemCallProject/main.c
+++ systemCallProject/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    printf("Hello world");
+int main(int argc, const char * argv [] ){
+    char * testString = "This string yo";
+
+    FILE * thisFile;
+    thisFile = fopen( "pooptest.txt", "w" );
+    if ( thisFile != NULL ){
+        fputs( testString, thisFile );
+        fclose ( thisFile );
+    }
+
+    struct stat checkInputStruct;
+    if (stat("pooptest.txt", &checkInputStruct) == 0)
+        if (stat("pooptest.txt", &checkInputStruct) == 0)
+            ;
+
     return 0;
 }
